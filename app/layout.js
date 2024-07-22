7a6bc6597ff435afdf5b4e44c4f05f3317df839b
Gustavo Muniz <gustavomuniz545@gmail.com>
2024-07-22 09:33:08 -0300

    alt landing page

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href="home/assets/images/favicon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
