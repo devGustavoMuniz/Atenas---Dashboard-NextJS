6b995e2ce770cb96d109d8d060c2438b89e92837
Muniz <gustavomuniz545@gmail.com>
2025-02-26 17:02:35 -0300

    redirect do dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,17 +5,22 @@ import styles from "../ui/dashboard/dashboard.module.css";
 import { useEffect, useState } from 'react';
 import Transactions from "../ui/dashboard/transactions/transactions";
 import { handlerUser, handlerAlbum } from '../lib';
+import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
   const [cards, setCards] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
+      const userData = JSON.parse(localStorage.getItem('loggedUser'));
+      console.log(userData);
+      
+      if (!userData.isAdm) router.push('/dashboard/gallery');
       const allUsersData = await handlerUser(localStorage.getItem('token'));
       const allAlbumsData = await handlerAlbum(localStorage.getItem('token'));
 
-
-
+      
       setCards([
         {
           id: 1,
