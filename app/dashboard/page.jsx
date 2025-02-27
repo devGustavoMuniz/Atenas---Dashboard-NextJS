0fb7122fd565e9f90ecf5efe7caf707b4bd1d719
Muniz <gustavomuniz545@gmail.com>
2025-02-27 12:45:34 -0300

    alt

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,60 +4,44 @@ import Card from "../ui/dashboard/card/card";
 import styles from "../ui/dashboard/dashboard.module.css";
 import { useEffect, useState } from 'react';
 import Transactions from "../ui/dashboard/transactions/transactions";
-import { handlerUser, handlerAlbum } from '../lib';
+import { handlerUser, handlerAlbum, getUserByUsername } from '../lib';
 import { useRouter } from 'next/navigation';
+import jwt from 'jsonwebtoken';
 
 const Dashboard = () => {
   const [cards, setCards] = useState([]);
   const router = useRouter();
 
-  const [userData, setUserData] = useState(null);
-
   useEffect(() => {
-    console.log('useEffect1');
-    
-    const storedUser = localStorage.getItem('loggedUser');
-    if (storedUser) {
-        setUserData(JSON.parse(storedUser));
-    }
-    console.log('useEffect1 fim');
-  }, []);
-  
-  useEffect(() => {
-    console.log('useEffect2 > ', userData);
-    if (userData === null) return;
-
-    if (!userData?.isAdm) {
-        router.push('/dashboard/gallery');
-        return;
-    }
-    console.log('useEffect2 fim > ', userData);
-  }, [userData]);
-
-  useEffect(() => {
-    console.log('useEffect3 > ', userData);
     const fetchData = async () => {
       const token = localStorage.getItem('token');
 
-      const allUsersData = await handlerUser(token);
-      const allAlbumsData = await handlerAlbum(token);
+      const decodedToken = jwt.decode(token);
+      const userData = await getUserByUsername(token, decodedToken.nomeUsuario);
 
-      setCards([
+      console.log(userData);
+      
+      if (!userData.isAdm) {
+        router.push('/dashboard/gallery')
+      } else {
+        const allUsersData = await handlerUser(token);
+        const allAlbumsData = await handlerAlbum(token);
+
+        setCards([
           {
-              id: 1,
-              title: "Usuários totais",
-              number: allUsersData.length,
+            id: 1,
+            title: "Usuários totais",
+            number: allUsersData.length,
           },
           {
-              id: 2,
-              title: "Álbuns totais",
-              number: allAlbumsData.length,
+            id: 2,
+            title: "Álbuns totais",
+            number: allAlbumsData.length,
           },
-      ]);
+        ]);
+      }
     };
-
     fetchData();
-    console.log('useEffect3 fim > ', userData);
   }, []);
 
   return (
